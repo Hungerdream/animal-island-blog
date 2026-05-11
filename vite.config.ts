822319f4b4cce10820a1cecab4439a0066c59b13
guokaigdg <guokaigdg@gmail.com>
2026-05-12 00:25:33 +0800

    trigger deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/animal-blog/",
+  base: "/animal-island-blog/",
   plugins: [
     react(),
     svgr({
